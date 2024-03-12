10964e66b6d2c330c2be47b097b7e3f3af63f5bb
Yerlan Kaliyev <kaliev.erlan.005@gmail.com>
2024-03-12 17:33:27 +0500

    lab6

---
+++ Lab6/src/app/album-detail/album-detail.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import {ActivatedRoute, RouterModule} from "@angular/router";
+import {AlbumsService} from "../albums.service";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-album-detail',
+  standalone: true,
+  imports: [RouterModule, CommonModule, FormsModule],
+  templateUrl: './album-detail.component.html',
+  styleUrl: './album-detail.component.scss'
+})
+export class AlbumDetailComponent {
+  albumId: number = 0;
+  album: any;
+  editedTitle: string = "";
+
+  constructor(private route: ActivatedRoute, private albumsService: AlbumsService) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.albumId = +params['id'];
+      this.albumsService.getAlbumById(this.albumId).subscribe(album => {
+        this.album = album;
+        this.editedTitle = album.title;
+      });
+    });
+  }
+
+  saveChanges() {
+    this.album.title = this.editedTitle;
+    this.albumsService.updateAlbumTitle(this.albumId, this.editedTitle).subscribe(() => {
+      console.log('Album title updated successfully');
+    });
+  }
+
+}
